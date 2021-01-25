a40f92e3216b9a21d95be5034d655d2dea31a6da
Ivan Ditchon <ivnditchon@gmail.com>
2021-01-26 00:15:56 +0800

    mobile responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,17 @@ const toggleMenu = () => {
     const menu = document.querySelector('#menu');
     const nav = document.querySelector('#nav');
     const mobileRes = document.querySelector('#mobile__res');
+    const createAcc = document.querySelector('.create__acc');
     
     menu.addEventListener('click', () => {
         nav.classList.toggle("nav__active");
         mobileRes.classList.toggle("mobile__res__active");
+        createAcc.classList.toggle("nav__item__active");
     }); 
 };
 
-toggleMenu();
+const myFunctions = () => {
+    toggleMenu();
+};
+
+myFunctions();
